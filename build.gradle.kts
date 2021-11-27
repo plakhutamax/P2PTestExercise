77adf52a9c74c8151672658741aab2576e52752c
Maksim Plakhuta <m.plakhuta@ivideon.com>
2021-11-27 12:37:10 +0300

    Migrate to KTS

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://maven.google.com/")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
